6002a736ebcf1981e1465c6c2abadc49d42a293a
439413423 <22100582@handong.ac.kr>
2024-12-02 22:08:34 +0900

    .

--- api/books.js
+++ api/books.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   try {
     const response = await axios.get(
-      "http://openapi.seoul.go.kr:8088/58624c767a63796c37386a42726a66/xml/SeoulLibraryBookSearchInfo/1/10"
+      "http://openapi.seoul.go.kr:8088/58624c767a63796c37386a42726a66/xml/SeoulLibraryBookSearchInfo/1/40"
     );
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).send(response.data); 

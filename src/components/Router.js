9d082e2a258384d22a68ebd3a73b85141d58f0f2
439413423 <22100582@handong.ac.kr>
2024-12-02 09:49:02 +0900

    .

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ShowList from "./Pages/ShowList";
-import EditUser from "./User/EditUser";
+import CartList from "./Pages/CartList";
 import Detail from "./Pages/Detail";
-import CreateUser from "./User/CreateUser";
+import RentalList from "./User/RentalList";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/list" replace />} />
-        <Route path="/list" element={<ShowList />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/update" element={<EditUser />} />
-        <Route path="/create" element={<CreateUser />} />
+        <Route path="/home" element={<ShowList />} />
+        <Route path="/book" element={<Detail />} />
+        <Route path="/cart" element={<CartList />} />
+        <Route path="/rental" element={<RentalList />} />
       </Routes>
     </BrowserRouter>
   );

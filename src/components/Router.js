63bca6d1727c61b16e47404ed743033d379c12bf
439413423 <22100582@handong.ac.kr>
2024-12-02 14:56:08 +0900

    .

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,16 +5,18 @@ import CartList from "./Pages/CartList";
 import Detail from "./Pages/Detail";
 import RentalList from "./Pages/RentalList";
 
-export default function Router() {
+const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<ShowList />} />
-        <Route path="/book" element={<Detail />} />
         <Route path="/cart" element={<CartList />} />
+        <Route path="/book" element={<Detail />} />
         <Route path="/rental" element={<RentalList />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
+
+export default Router;
